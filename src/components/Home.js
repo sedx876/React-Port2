a7b7342499874c064e79a4a4ab7552c82ab79201
sedx876 <swatkins876@gmail.com>
2020-12-10 07:27:44 -0500

    work on home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import image from '../raincomp.jpg'
 
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
+    <main>
+      <img src={image} alt="rainbow computer" className='absolute object-cover w-full h-full' />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
+        <h1 className="text-4xl text-teal-100 font-bold cursive leading-none lg:leading-snug home-name justify-center">Hello...I'm Sharon</h1>
+      </section>
+    </main>
   )
 }
 
