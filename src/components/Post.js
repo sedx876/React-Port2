3d97e1cc61ecaf1766f3b4eb00b84b6d54cf978f
sedx876 <swatkins876@gmail.com>
2020-12-10 09:33:10 -0500

    work on post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,11 +1,53 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import sanityClient from '../client.js'
 
 const Post = () => {
-  return (
-    <div>
-      <h1>Post</h1>
-    </div>
-  )
+  const [postData, setPost] = useState(null)
+
+  useEffect(() => {
+    sanityClient 
+      .fetch(`*[_type == "post"] {
+        title,
+        slug,
+        mainImage{
+          asset->{
+            _id,
+            url
+          },
+          alt
+        }
+      }`)
+      .then((data) => setPost(data))
+      .catch(console.error)
+  }, [] )
+
+  return(
+    <main className="bg-teal-100 min-h-screen p-12">
+      <section className="container mx-auto">
+        <h1 className="text-5xl text-teal-900 flex justify-center cursive">Blog Posts</h1>
+        <h2 className="text-3xl text-teal-900 flex justify-center cursive mb-12">Welcome To My Blog Posts Page</h2>
+        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
+          {postData && postData.map((post, index) =>(
+            
+          
+          <article>
+            <Link to={"/post/" + post.slug.current} key={post.slug.current}>
+            <span className="block h-64 relative rounded shadow leading-snug bg-white border-l-8 border-teal-400" key={index}>
+              <img src={post.mainImage.asset.url} 
+              alt={post.mainImage.alt}
+              className="w-full h-full rounded-r object-cover absolute" />
+              <span className="block relative h-full flex justify-end items-end pr-4 pb-4">
+                <h3 className="text-teal-900 text-lg font-bold px-3 py-4 bg-teal-700 text-teal-100 bg-opacity-75 rounded">{post.title}</h3>
+              </span>
+            </span>
+            </Link>
+          </article>
+          ))}
+        </div>
+      </section>
+    </main>
+  ) 
 }
 
 export default Post
