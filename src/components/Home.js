631cfa4689ae829d4156f49bdea39b08a43dbc78
sedx876 <swatkins876@gmail.com>
2020-12-11 06:01:55 -0500

    code cleanup

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,9 +4,29 @@ import image from '../raincomp.jpg'
 const Home = () => {
   return (
     <main>
-      <img src={image} alt="rainbow computer" className='absolute object-cover w-full h-full' />
-      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8">
-        <h1 className="text-4xl text-teal-100 font-bold cursive leading-none lg:leading-snug home-name justify-center">Hello...I'm Sharon</h1>
+      <img src={image} 
+      alt="rainbow computer" 
+      className='absolute 
+      object-cover 
+      w-full 
+      h-full' />
+
+      <section className="relative 
+      flex 
+      justify-center 
+      min-h-screen 
+      pt-12 
+      lg:pt-64 
+      px-8">
+
+        <h1 className="text-4xl 
+        text-teal-100 
+        font-bold 
+        cursive 
+        leading-none 
+        lg:leading-snug 
+        home-name 
+        justify-center">Hello...I'm Sharon</h1>
       </section>
     </main>
   )
